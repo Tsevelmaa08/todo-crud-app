c9394c6f893a78916e9d24d42f26da7391538242
Tsevelmaa08 <enkhtortsevelmaa605@gmail.com>
2026-06-09 15:31:50 +0900

    app.py

--- app.py
+++ app.py
@@ -1,29 +1,67 @@
+import sqlite3
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-tasks = []
+# DB initialize
+def init_db():
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        content TEXT
+    )
+    """)
+    conn.commit()
+    conn.close()
 
+init_db()
+
+# HOME
 @app.route("/", methods=["GET", "POST"])
 def index():
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+
     if request.method == "POST":
         task = request.form["task"]
-        tasks.append(task)
-        return redirect("/")
+        c.execute("INSERT INTO tasks (content) VALUES (?)", (task,))
+        conn.commit()
+
+    c.execute("SELECT * FROM tasks")
+    tasks = c.fetchall()
+    conn.close()
+
     return render_template("index.html", tasks=tasks)
 
+# DELETE
 @app.route("/delete/<int:id>")
 def delete(id):
-    tasks.pop(id)
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+    c.execute("DELETE FROM tasks WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
     return redirect("/")
 
+# EDIT
 @app.route("/edit/<int:id>", methods=["GET", "POST"])
 def edit(id):
+    conn = sqlite3.connect("tasks.db")
+    c = conn.cursor()
+
     if request.method == "POST":
         new_task = request.form["task"]
-        tasks[id] = new_task
+        c.execute("UPDATE tasks SET content=? WHERE id=?", (new_task, id))
+        conn.commit()
+        conn.close()
         return redirect("/")
-    return render_template("edit.html", task=tasks[id], id=id)
+
+    c.execute("SELECT * FROM tasks WHERE id=?", (id,))
+    task = c.fetchone()
+    conn.close()
+    return render_template("edit.html", task=task)
 
 if __name__ == "__main__":
     app.run(debug=True)
